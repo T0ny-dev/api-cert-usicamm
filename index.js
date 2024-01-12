d6e5a69a854ca03f2f83e644c3db311bb1d0d8e6
T0ny-dev <antonioraweb@gmail.com>
2024-01-12 00:05:02 -0600

    update route id cert

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ app.get('/certificates', (req, res) => {
     });
 });
 
+// Ruta para obtener un certificado específico por su ID
+app.get('/certificates/:id', (req, res) => {
+  const certificates = [];
+
+  fs.createReadStream('data.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      certificates.push(row);
+    })
+    .on('end', () => {
+      const certId = req.params.id;
+
+      const certificate = certificates.find((cert) => cert.Folio === certId);
+
+      if (certificate) {
+        res.json(certificate);
+      } else {
+        res.status(404).json({ error: 'Certificado no encontrado' });
+      }
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor API corriendo en http://localhost:${PORT}`);
 });
